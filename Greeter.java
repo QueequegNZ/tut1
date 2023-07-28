592db1294c4db30ea1a33a7ae26876012bb79458
David Jaquiery <djaquiery@gmail.com>
2023-07-29 06:11:56 +1200

    Added feature 2

--- Greeter.java
+++ Greeter.java
@@ -13,7 +13,7 @@ public class Greeter {
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greetings") + ", " + name;
+    return messages.getString("greetings") + ", " + `name;' to `" "+name+" ";
   }
 
   public static void main(String[] args) {
